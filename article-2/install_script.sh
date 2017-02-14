37ec1e88b74e72da532af5991fca1be4090b13a3
Ilya N. Zykin <zykin-ilya@ya.ru>
2017-02-14 23:34:34 +0400

    Update install_script.sh

--- article-2/install_script.sh
+++ article-2/install_script.sh
@@ -1,11 +1,36 @@
+#!/bin/bash
+
 # INSTALLATION SCRIPT FOR:
 #
-# Linux 3.16.0-4-amd64
-# 1 SMP Debian 3.16.7-ckt25-2+deb8u3 (2016-07-02) x86_64 GNU/Linux
+# Linux 3.16.0-4-amd64 x86_64
+#
 # PRETTY_NAME="Debian GNU/Linux 8 (jessie)"
 # NAME="Debian GNU/Linux"
 # VERSION_ID="8"
 # VERSION="8 (jessie)"
+# ID=debian
+
+uname -srm
+cat /etc/*-release
+
+# VERSIONS
+
+NODE_VERSION=7
+SPHINX_VERSION=2.2.11-release-1
+
+DEPLOYER_RVM_RUBY_VERSION=ruby-2.3.3
+DEPLOYER_RVM_GEMSET_NAME=rails_app
+
+# DEFINE PASSWORDS
+
+ROOT_PASSWORD=`openssl rand -hex 15`
+DEPLOYER_PASSWORD=`openssl rand -hex 15`
+
+ROOT_MYSQL_PASSWORD=`openssl rand -hex 15`
+DEPLOYER_MYSQL_PASSWORD=`openssl rand -hex 15`
+
+ROOT_PSQL_PASSWORD=`openssl rand -hex 15`
+DEPLOYER_PSQL_PASSWORD=`openssl rand -hex 15`
 
 # SET LANG VARS
 
@@ -16,35 +41,73 @@ echo 'export LC_CTYPE="en_US.UTF-8"' >> ~/.bashrc
 
 source ~/.bashrc
 
-# ADD USER
+# VIM OPTIONS
 
-adduser rails --home /home/rails --shell /bin/bash --disabled-password --gecos ''
-echo "rails:qwerty12345" | chpasswd
-
-echo "set nocompatible" >> ~/.vimrc
+echo "set nocompatible" > ~/.vimrc
 echo ":set backspace=indent,eol,start" >> ~/.vimrc
 
-# Folders & Files for user `rails`
+# APT UPDATE AND CORE UTILS
 
-# for ssh
+mv /etc/apt/sources.list /etc/apt/sources.list.backup
+
+echo "deb http://httpredir.debian.org/debian/ stable main contrib non-free
+deb http://httpredir.debian.org/debian/ stable-updates main contrib non-free
+deb http://security.debian.org/ stable/updates main contrib non-free
+deb-src http://httpredir.debian.org/debian/ stable main contrib non-free
+deb-src http://httpredir.debian.org/debian/ stable-updates main contrib non-free
+deb-src http://security.debian.org/ stable/updates main contrib non-free
+deb http://httpredir.debian.org/debian jessie-backports main contrib non-free
+deb-src http://httpredir.debian.org/debian jessie-backports main contrib non-free
+" > /etc/apt/sources.list
+
+# ALT WAY
+# wget http://debian.mirror.vu.lt/debian-sources.list && mv debian-sources.list /etc/apt/sources.list
+
+apt-get clean
+apt-get update && apt-get install -y coreutils
+
+##################################################################
+# `ROOT` USER
+##################################################################
+
+echo "root:$ROOT_PASSWORD" | chpasswd
+
+##################################################################
+# `RAILS` USER
+##################################################################
+
+adduser rails --home /home/rails --shell /bin/bash --disabled-password --gecos ''
+echo "rails:$DEPLOYER_PASSWORD" | chpasswd
+chown -R rails:rails /home/rails/
+
+# FOR SSH
+
 mkdir -p /home/rails/.ssh
 chown rails:rails /home/rails/.ssh
+chmod 0700 /home/rails/.ssh
 
-# for Nginx
+# FOR NGINX
+
 mkdir -p /home/rails/www
 chown rails:rails /home/rails/www
-chmod -R 0755 /home/rails/www
+chmod 0755 /home/rails/www
 
-# for gem install
+# FOR GEM INSTALL
+
 echo 'gem: --no-document' >> /home/rails/.gemrc
 chown rails:rails /home/rails/.gemrc
 chmod -R 0644 /home/rails/.gemrc
 
-# for vim
+# FOR VIM
+
 echo "set nocompatible" >> /home/rails/.vimrc
 echo ":set backspace=indent,eol,start" >> /home/rails/.vimrc
 chown rails:rails /home/rails/.vimrc
-chmod -R 0644 /home/rails/.vimrc
+chmod 0644 /home/rails/.vimrc
+
+##################################################################
+# `RAILS` USER
+##################################################################
 
 # CREATE SWAP
 
@@ -57,75 +120,114 @@ echo "/swapfile none swap sw 0 0" >> /etc/fstab
 
 echo 10 | tee /proc/sys/vm/swappiness
 echo vm.swappiness = 10 | tee -a /etc/sysctl.conf
+sysctl --system
 
-# BASE SOFT
-mv /etc/apt/sources.list /etc/apt/sources.list.original
-wget http://debian.mirror.vu.lt/debian-sources.list && mv debian-sources.list /etc/apt/sources.list
+# MINIMAL SOFT PACK
 
-apt-get clean
-apt-get update
-apt-get install build-essential autoconf bison checkinstall curl git-core libodbc1 libc6-dev libreadline6 libreadline6-dev libsqlite3-0 libsqlite3-dev libssl-dev libxml2 libxml2-dev libxslt-dev libxslt1-dev libxslt1.1 libyaml-dev openssl sqlite3 zlib1g zlib1g-dev htop -y
+apt-get install -y sudo screen dialog apt-transport-https ca-certificates \
+  man-db deborphan aptitude bc bash-completion command-not-found \
+  python-software-properties htop nmon iotop dstat vnstat unzip zip unar pigz \
+  p7zip-full logrotate wget curl w3m lftp rsync openssh-server telnet nano mc \
+  pv less sysstat ncdu ethtool dnsutils mtr-tiny rkhunter ntpdate ntp vim
 
-# New Relic Monitor
+# REQUIREMENTS LIBS
+
+apt-get install -y build-essential autoconf bison checkinstall git-core \
+  libodbc1 libc6-dev libreadline6 libreadline6-dev libsqlite3-0 libsqlite3-dev \
+  libssl-dev libxml2 libxml2-dev libxslt-dev libxslt1-dev libxslt1.1 \
+  libyaml-dev openssl sqlite3 zlib1g zlib1g-dev
+
+# NEW RELIC MONITOR
 
 echo 'deb http://apt.newrelic.com/debian/ newrelic non-free' | tee /etc/apt/sources.list.d/newrelic.list
 wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add -
 apt-get update
-apt-get install newrelic-sysmond
+apt-get install -y newrelic-sysmond
 
 # IMAGE OPTIMIZERS
 
-apt-get install gifsicle jhead jpegoptim libjpeg-progs optipng pngcrush pngquant -y
+apt-get install -y gifsicle jhead jpegoptim libjpeg-progs optipng pngcrush pngquant
 
 cd /tmp/
 
 wget http://static.jonof.id.au/dl/kenutils/pngout-20150319-linux.tar.gz
 tar -xvf pngout-20150319-linux.tar.gz
-cp /tmp/pngout-20150319-linux/x86_64/pngout /usr/bin/pngout
+cp /tmp/pngout-20150319-linux/x86_64/pngout /usr/local/bin/pngout
 
 rm -rf ./pngout-20150319*
 cd ~
 
 # IMAGE MAGICK
 
-apt-get install imagemagick libmagickwand-dev -y
+apt-get install -y imagemagick libmagickwand-dev
 convert --version
 
 # NODE
 # https://github.com/nodesource/distributions
 
-curl -sL https://deb.nodesource.com/setup_6.x | bash -
-apt-get install nodejs -y
+curl -sL https://deb.nodesource.com/setup_$NODE_VERSION.x | bash -
+apt-get install -y nodejs
 
 # REDIS
 
-cd /tmp/
-
-wget http://download.redis.io/releases/redis-3.2.3.tar.gz
-tar -zxvf redis-3.2.3.tar.gz
-cd /tmp/redis-3.2.3
-
-make
-checkinstall --pkgname=redis-server --pkgversion "3.2.3" --default
-
-cd ..
-rm -rf ./redis-3.2.3*
-cd ~
+apt-get install -y redis-server
 
 # NGINX
 
-cd /tmp
+curl http://nginx.org/keys/nginx_signing.key | apt-key add -
 
-wget http://nginx.org/keys/nginx_signing.key
-apt-key add nginx_signing.key
-
-echo 'deb http://nginx.org/packages/debian/ jessie nginx'     >> /etc/apt/sources.list
-echo 'deb-src http://nginx.org/packages/debian/ jessie nginx' >> /etc/apt/sources.list
+echo 'deb http://nginx.org/packages/debian/ jessie nginx'     >> /etc/apt/sources.list.d/nginx.list
+echo 'deb-src http://nginx.org/packages/debian/ jessie nginx' >> /etc/apt/sources.list.d/nginx.list
 
 apt-get update
-apt-get install nginx
+apt-get install -y nginx
 
-rm -rf ./nginx*
+# PYTHON / PIP / PYGMENTS
+
+apt-get install -y python-pip
+pip install --upgrade Pygments
+
+# MYSQL
+
+export DEBIAN_FRONTEND=noninteractive
+
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $ROOT_MYSQL_PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $ROOT_MYSQL_PASSWORD"
+
+apt-get install -y mysql-server mysql-common mysql-client libmysqlclient-dev ruby-mysql
+
+unset DEBIAN_FRONTEND
+
+echo "[client]
+user=root
+password=$ROOT_MYSQL_PASSWORD
+" >> ~/.my.cnf
+
+chmod 600 ~/.my.cnf
+
+mysql -D mysql -r -B -N -e "CREATE USER 'rails'@'localhost' IDENTIFIED BY '$DEPLOYER_MYSQL_PASSWORD'"
+mysql -D mysql -r -B -N -e "CREATE DATABASE rails_app_db CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -D mysql -r -B -N -e "GRANT ALL PRIVILEGES ON rails_app_db.* TO 'rails'@'localhost' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0"
+mysql -D mysql -r -B -N -e "SHOW GRANTS FOR 'rails'@'localhost'"
+
+# PSQL
+
+# apt-cache pkgnames postgresql
+apt-get install -y postgresql libpq-dev
+
+su -s /bin/bash -l postgres -c "psql -U postgres -c \"CREATE USER rails WITH PASSWORD '$DEPLOYER_PSQL_PASSWORD';\""
+su -s /bin/bash -l postgres -c "psql -U postgres -c \"ALTER ROLE rails WITH CREATEDB;\""
+
+su -s /bin/bash -l postgres -c "createdb -E UTF8 -O rails rails_app_db"
+su -s /bin/bash -l postgres -c "psql -U postgres -c \"GRANT ALL PRIVILEGES ON DATABASE rails_app_db TO rails;\""
+
+# SPHINX SEARCH
+
+cd /tmp
+
+wget http://sphinxsearch.com/files/sphinxsearch_$SPHINX_VERSION~jessie_amd64.deb
+dpkg -i sphinxsearch_$SPHINX_VERSION~jessie_amd64.deb
+rm -rf ./sphinxsearch*
 cd ~
 
 # RVM REQUIREMENTS
@@ -136,51 +238,6 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 /usr/local/rvm/bin/rvm requirements
 
 (echo 'yes') | (/usr/local/rvm/bin/rvm implode)
-
-# MYSQL
-
-export DEBIAN_FRONTEND=noninteractive
-
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password qwerty12345'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password qwerty12345'
-
-apt-get install mysql-server mysql-common mysql-client libmysqlclient-dev ruby-mysql -y
-
-unset DEBIAN_FRONTEND
-
-mysql -u root -pqwerty12345 -D mysql -r -B -N -e "CREATE USER 'rails'@'localhost' IDENTIFIED BY 'qwerty12345'"
-mysql -u root -pqwerty12345 -D mysql -r -B -N -e "CREATE DATABASE rails_app_db CHARACTER SET utf8 COLLATE utf8_general_ci;"
-mysql -u root -pqwerty12345 -D mysql -r -B -N -e "GRANT ALL PRIVILEGES ON rails_app_db.* TO 'rails'@'localhost' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0"
-mysql -u root -pqwerty12345 -D mysql -r -B -N -e "SHOW GRANTS FOR 'rails'@'localhost'"
-
-# PSQL
-# apt-cache pkgnames postgresql
-
-apt-get install postgresql-9.4 postgresql-server-dev-9.4 libpq-dev -y
-
-su -s /bin/bash -l postgres -c "psql -U postgres -c \"CREATE USER rails WITH PASSWORD 'qwerty12345';\""
-su -s /bin/bash -l postgres -c "psql -U postgres -c \"ALTER ROLE rails WITH CREATEDB;\""
-
-su -s /bin/bash -l postgres -c "createdb -E UTF8 -O rails rails_app_db"
-su -s /bin/bash -l postgres -c "psql -U postgres -c \"GRANT ALL PRIVILEGES ON DATABASE rails_app_db TO rails;\""
-
-# Pyhon / pip / Pygments
-
-apt-get install python-pip -y
-pip install --upgrade Pygments
-
-# pip list | grep Pygments
-# which pygmentize
-
-# SPHINX SEARCH
-
-cd /tmp
-
-wget http://sphinxsearch.com/files/sphinxsearch_2.2.11-release-1~jessie_amd64.deb
-dpkg -i sphinxsearch_2.2.11-release-1~jessie_amd64.deb
-
-rm -rf ./sphinxsearch*
-cd ~
 
 ### RAILS USER ###
 
@@ -193,6 +250,72 @@ su -s /bin/bash -l rails -c "source ~/.bashrc"
 su -s /bin/bash -l rails -c "\curl -sSL https://get.rvm.io | bash"
 su -s /bin/bash -l rails -c "source ~/.bash_profile"
 su -s /bin/bash -l rails -c "which rvm"
-su -s /bin/bash -l rails -c "rvm install ruby-2.2.4"
-su -s /bin/bash -l rails -c "(rvm use ruby-2.2.4) && (rvm gemset use rails_app --create)"
-su -s /bin/bash -l rails -c "rvm ruby-2.2.4@rails_app do gem install bundler"
+su -s /bin/bash -l rails -c "rvm autolibs disable & rvm install $DEPLOYER_RVM_RUBY_VERSION"
+su -s /bin/bash -l rails -c "(rvm use $DEPLOYER_RVM_RUBY_VERSION) && (rvm gemset use $DEPLOYER_RVM_GEMSET_NAME --create)"
+su -s /bin/bash -l rails -c "rvm $DEPLOYER_RVM_RUBY_VERSION@$DEPLOYER_RVM_GEMSET_NAME do gem install bundler"
+
+echo "--------------------------------------------------" >> ~/credentials.txt
+echo "ROOT CREDENTIALS"                                   >> ~/credentials.txt
+echo "--------------------------------------------------" >> ~/credentials.txt
+
+echo "
+DROPLET:
+  username: root
+  password: $ROOT_PASSWORD
+
+POSTGRESQL
+  username: root
+  password: $ROOT_PSQL_PASSWORD
+
+MYSQL
+  username: root
+  password: $ROOT_MYSQL_PASSWORD
+" >> ~/credentials.txt
+
+echo "--------------------------------------------------" >> /home/rails/credentials.txt
+echo "DEPLOYER LOGIN/PASSWORD"                            >> /home/rails/credentials.txt
+echo "--------------------------------------------------" >> /home/rails/credentials.txt
+
+echo "
+LOGIN: rails
+PASSWORD: $DEPLOYER_PASSWORD
+"  >> /home/rails/credentials.txt
+
+echo "--------------------------------------------------" >> /home/rails/credentials.txt
+echo "PSQL DATABASE CONNECTION PARAMS"                    >> /home/rails/credentials.txt
+echo "--------------------------------------------------" >> /home/rails/credentials.txt
+
+echo "
+production:
+  adapter: postgresql
+  encoding: unicode
+  pool: 5
+  username: rails
+  database: rails_app_db
+  password: $DEPLOYER_PSQL_PASSWORD
+" >> /home/rails/credentials.txt
+
+echo "--------------------------------------------------" >> /home/rails/credentials.txt
+echo "MYSQL DATABASE CONNECTION PARAMS"                   >> /home/rails/credentials.txt
+echo "--------------------------------------------------" >> /home/rails/credentials.txt
+
+echo "
+production:
+  adapter: mysql2
+  encoding: utf8
+  pool: 5
+  username: rails
+  database: rails_app_db
+  password: $DEPLOYER_MYSQL_PASSWORD
+"  >> /home/rails/credentials.txt
+
+echo "--------------------------------------------------" >> /home/rails/credentials.txt
+echo "SERVER IP ADDRESS PARAMS"                           >> /home/rails/credentials.txt
+echo "--------------------------------------------------" >> /home/rails/credentials.txt
+
+echo "" >> /home/rails/credentials.txt
+
+ifconfig eth0 | grep inet | awk "{print $2}" | sed "s/addr://" >> /home/rails/credentials.txt
+
+cat ~/credentials.txt
+cat /home/rails/credentials.txt
